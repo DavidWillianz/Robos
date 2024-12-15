7367e8f9148a71e8ab0a520118a1237d7ffa32f0
David Willian <davidvieir22017@gmail.com>
2024-12-15 14:16:07 -0300

    FIX[robos]: Organização de pastas e arquivos do projeto

--- Robos/busca_dominios/busca_dominios.py
+++ Robos/busca_dominios/busca_dominios.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-import pandas as pd
 import time
 import os
 
